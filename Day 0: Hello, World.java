3760bc219a30ae5d5de34d02eb488ba976eadc6a
Aman Eleven <hackerr647@gmail.com>
2022-03-13 00:21:34 +0530

    Create Day 0: Hello, World.java

---
+++ Day 0: Hello, World.java
@@ -0,0 +1,12 @@
+//JAVA 15
+ 
+
+Scanner sc=new Scanner(System.in);
+        System.out.println("Hello, World.");
+        String s;
+        s=sc.nextLine();
+        System.out.println(s);
+
+
+//Author : AmanEleven
+//Follow : All Personal and Professional Links Here -->  https://linktr.ee/amaneleven
