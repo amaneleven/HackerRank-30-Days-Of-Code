fdf793318422e1a7b057b2d7129b67821879830e
Aman Eleven <hackerr647@gmail.com>
2022-03-13 13:55:58 +0530

    Create Day 1: Data Types.java

---
+++ Day 1: Data Types.java
@@ -0,0 +1,19 @@
+ Scanner sc=new Scanner(System.in);
+    int i=4;
+    double d= 4.0;
+    String s = "HackerRank ";
+    
+    int a=sc.nextInt();
+    double b =sc.nextDouble();
+    sc.nextLine();
+    String c =sc.nextLine();
+    
+   
+//Author : AmanEleven
+//Follow : All Personal and Professional Links Here -->  https://linktr.ee/amaneleven
+    
+    System.out.println(a+i);
+    System.out.println(b+d);
+    System.out.println(s+c);
+    
+    
