9fbb95cfeac4eeb2250792ac0462abfce9369473
Aman Eleven <hackerr647@gmail.com>
2022-03-15 09:24:36 +0530

    Create Day 2: Operators.java

---
+++ Day 2: Operators.java
@@ -0,0 +1,9 @@
+double tip=(meal_cost*tip_percent)/100;
+        double tax=(meal_cost*tax_percent)/100;
+        int cost=(int)Math.round(meal_cost+tip+tax);
+        System.out.println(cost);
+
+
+//Author : AmanEleven
+//Follow : All Personal and Professional Links Here -->  https://linktr.ee/amaneleven
+//as simple as fuck #lol
