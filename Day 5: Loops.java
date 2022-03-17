634acb7addc5bc7f63329f9a0c03c4af6cc4e461
Aman Eleven <hackerr647@gmail.com>
2022-03-17 13:09:05 +0530

    Create Day 5: Loops.java

---
+++ Day 5: Loops.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n, i ,s=0;
+        n=sc.nextInt();
+        for(i=1;i<=10;i++){
+             System.out.format("%d x %d = %d%n", n, i , i * n);
+        } 
+    }
+}
+
+//Author : AmanEleven
+//Follow : All Personal and Professional Links Here -->  https://linktr.ee/amaneleven
