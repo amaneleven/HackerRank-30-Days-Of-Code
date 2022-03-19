db60e768e0cd647aa8cb0e90d1112ff13cbbb041
Aman Eleven <hackerr647@gmail.com>
2022-03-19 10:27:53 +0530

    Create Day 7: Arrays.java

---
+++ Day 7: Arrays.java
@@ -0,0 +1,22 @@
+//DELETE ALL FROM CLASS SOULTUION AND JUST PASTE THIS CODE+============
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int []a=new int[10];
+        int n;
+        
+        n=sc.nextInt();
+        for(int i=0;i<n;i++){
+            a[i]=sc.nextInt();            
+        }
+       // int aL=a.length;
+        for(int i=n-1;i>=0;i--){
+            System.out.print(a[i]+" ");
+        }
+        
+//Author : AmanEleven
+//Follow : All Personal and Professional Links Here -->  https://linktr.ee/amaneleven     
+        
+    }
+}
